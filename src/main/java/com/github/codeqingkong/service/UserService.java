a8476cca96b07fcbb497c5f63114ebf591fc288c
LuoZhiWei <1031556593@qq.com>
2021-12-07 22:40:02 +0800

    添加user增删改查

---
+++ src/main/java/com/github/codeqingkong/service/UserService.java
@@ -0,0 +1,38 @@
+package com.github.codeqingkong.service;
+
+import com.github.codeqingkong.model.User;
+
+/**
+ * @author qingkong
+ * @date 2021/12/7
+ */
+public interface UserService {
+    /**
+     * 新增用户
+     * @param user
+     * @return
+     */
+    boolean insertUser(User user);
+
+    /**
+     * 根据 userId 删除一个用户
+     * @param userId
+     * @return
+     */
+    boolean deleteUser(Integer userId);
+
+    /**
+     * 修改用户
+     * @param user
+     * @return
+     */
+    boolean updateUser(User user);
+
+    /**
+     * 根据 userId 查询用户
+     * @param userId
+     * @return
+     */
+    boolean getOneUser(Integer userId);
+
+}
---
+++ src/main/java/com/github/codeqingkong/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.github.codeqingkong.service.impl;
+
+import com.github.codeqingkong.model.User;
+import com.github.codeqingkong.service.UserService;
+
+/**
+ * @author qingkong
+ * @date 2021/12/7
+ */
+public class UserServiceImpl implements UserService {
+    @Override
+    public boolean insertUser(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteUser(Integer userId) {
+        return false;
+    }
+
+    @Override
+    public boolean updateUser(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean getOneUser(Integer userId) {
+        return false;
+    }
+}
