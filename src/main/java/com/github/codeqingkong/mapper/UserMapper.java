9c1be6d79793598b1dd20b187288cef1399a0210
LuoZhiWei <1031556593@qq.com>
2021-12-08 22:54:06 +0800

    添加user增删改查

---
+++ src/main/java/com/github/codeqingkong/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.github.codeqingkong.mapper;
+
+import com.github.codeqingkong.model.User;
+
+/**
+ * @author qingkong
+ * @date 2021/12/8
+ */
+public interface UserMapper {
+    int insertUser(User user);
+    int deleteUser(Integer userId);
+    int updateUser(User user);
+    User getOneUserById(Integer userId);
+}
