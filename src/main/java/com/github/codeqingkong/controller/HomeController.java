41f9443980cb92fad722b4e0fbc2e1f6214f179b
LoganLuo <logan_luo@mtelnet.com>
2022-07-22 21:10:40 +0800

    增加首页导航

--- src/main/java/com/github/codeqingkong/controller/HomeController.java
+++ src/main/java/com/github/codeqingkong/controller/HomeController.java
@@ -1,12 +1,20 @@
 package com.github.codeqingkong.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**首页控制器
  * @author qingkong
  * @date 2021/12/19
  */
 @Controller
+@RequestMapping("/home")
 public class HomeController {
-
+    @GetMapping("/index")
+    public ModelAndView index(ModelAndView modelAndView){
+        modelAndView.setViewName("");
+        return modelAndView;
+    }
 }
