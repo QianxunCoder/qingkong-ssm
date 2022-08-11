6722617d5930ab8313f2274d0de0110a95440d1d
LoganLuo <logan_luo@mtelnet.com>
2022-08-11 16:40:52 +0800

    增加用户详情页以及 UserVO.java

--- src/main/java/com/github/codeqingkong/controller/UserController.java
+++ src/main/java/com/github/codeqingkong/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.github.codeqingkong.controller;
 
-import com.github.codeqingkong.common.Result;
+import com.github.codeqingkong.vo.UserVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -23,5 +25,13 @@ public class UserController {
     public ModelAndView list(ModelAndView modelAndView){
         modelAndView.setViewName("user/list");
         return modelAndView;
+    }
+
+    @GetMapping("/detail")
+    @ResponseBody
+    public UserVO detail(@RequestParam("userId") Integer userId){
+        // 省略业务代码
+        UserVO userVO = new UserVO();
+        return userVO;
     }
 }
---
+++ src/main/java/com/github/codeqingkong/vo/UserVO.java
@@ -0,0 +1,14 @@
+package com.github.codeqingkong.vo;
+
+import com.github.codeqingkong.model.User;
+
+/**
+ * @author: LoganLuo
+ * @date: 2022/8/11
+ */
+public class UserVO extends User {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
