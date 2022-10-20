d93bc4c60e8f8240f04a4b0e943f60b4fde19c13
LoganLuo <logan_luo@mtelnet.com>
2022-10-20 20:48:11 +0800

    添加 RoleVO

---
+++ src/main/java/com/github/codeqingkong/vo/RoleVO.java
@@ -0,0 +1,14 @@
+package com.github.codeqingkong.vo;
+
+import com.github.codeqingkong.model.Role;
+
+/**
+ * @author: LoganLuo
+ * @date: 2022/10/20
+ */
+public class RoleVO extends Role {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
